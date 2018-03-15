09888660a8f7f2f34dbf30d8430afc34564167dd
Wojtek Gumulski <wojciech.gumulski@gmail.com>
2018-03-15 23:09:21 +0100

    Add files

---
+++ program.js
@@ -0,0 +1,21 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if (input !== null) {
+    	var instruction = input.toString().trim();
+    		switch (instruction) {
+				case '/exit':
+					process.stdout.write('Quitting app!\n');
+            		process.exit();
+        		case 'language':
+        			console.log(process.env.lang);
+        			break;
+        		case 'version':
+        			console.log(process.versions.node);
+        			break;
+        		default:
+		            process.stderr.write('Wrong instruction!\n');
+        	}
+    }
+});
